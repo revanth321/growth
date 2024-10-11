22bec4c14ff5733b7cbe875444512395f2e7ae5f
revanth321 <59685371+revanth321@users.noreply.github.com>
2024-10-12 00:29:58 +0530

    Updated authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,22 @@
 import asyncHandler from 'express-async-handler';
 import jsonwebtoken from 'jsonwebtoken';
 import User from '../models/userModel.js';
-
+// @desc       Protect routes (authentication middleware)
+// @route      Middleware function, no specific route
+// @access     Private (used for protecting routes)
 const protect = asyncHandler(async (req, res, next) => {
     let token;
     console.log(req);
+    // @desc   Extract the token from the request header
     token = req.header('jwt');
 
     if(token) {
         try {
+            // @desc   Verify the token
             const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+            // @desc   Find the user by decoded token's userId and attach to req object, exclude password
             req.user = await User.findById(decoded.userId).select('-password');
+            // @desc   Move to the next middleware/route
             next();
         } catch (error) {
             res.status(401);
